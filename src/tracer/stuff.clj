2fe099d8192360faf8a01477c81cfa66ac520496
Andrei Boltachev <andrewboltachev@gmail.com>
2017-09-24 22:31:29 +0300

    better debug

--- src/tracer/stuff.clj
+++ src/tracer/stuff.clj
@@ -17,22 +17,22 @@
 	 :prefix "123abc"
    :number 16})
 
-(defn debug [& args]
+(defn debug [expr]
 	(let [{:keys [uri prefix id]} *tracer-config*]
 		(client/put
 			(str uri prefix "-" (clojure.string/replace (.toString (java.util.Date.)) " " "_") "-" (rand-str 4))
 		  {:body (json/generate-string
                {:prefix prefix
                 :id id
-                :data (pr-str args)})
+                :data (pr-str expr)})
 		   :headers {}
   		 :socket-timeout 1000  ;; in milliseconds
 		   :conn-timeout 1000})))
 
-(defmacro dbgfn [f & args]
+(defmacro dbgfn [expr]
   `(do
-    (debug (str '~f) ~@args)
-    (~f ~@args)))
+    (debug '~expr)
+    ~expr))
 
 (go-loop
   []
@@ -41,5 +41,5 @@
 	  (let [data1 (<! stuff-ch)]
   	  (newline)
 	    (println ">>>" (.toString (new java.util.Date)))
-  	  (dbgfn cprint "Hello world!")))
+  	  (dbgfn (cprint "Hello world!"))))
     	(recur))

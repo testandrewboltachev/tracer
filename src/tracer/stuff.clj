7edf5b060958f9a88c61f1ff5cc42a772e626d84
Andrei Boltachev <andrewboltachev@gmail.com>
2017-11-24 21:05:41 +0300

    potemkin

--- src/tracer/stuff.clj
+++ src/tracer/stuff.clj
@@ -181,40 +181,6 @@
         r))
 
 
-(defn walk
-  "Like `clojure.walk/walk`, but preserves metadata."
-  [inner outer form]
-(println "walk" (class form)
-(cond
-            (list? form) 1
-            (instance? clojure.lang.IMapEntry form) 2
-            (seq? form) 3
-            (coll? form) 4
-            :else 5)
-
-)
-(cprint form)
-  (let [x (cond
-            (list? form) (outer (apply list (map inner form)))
-            (instance? clojure.lang.IMapEntry form) (outer (vec (map inner form)))
-            (seq? form) (outer (doall (map inner form)))
-            (coll? form) (outer (into (empty form) (map inner form)))
-            :else (outer form))]
-    (if (instance? clojure.lang.IObj x)
-      (with-meta x (merge (meta form) (meta x)))
-      x)))
-
-(defn postwalk
-  "Like `clojure.walk/postwalk`, but preserves metadata."
-  [f form]
-  (walk (partial postwalk f) f form))
-
-(defn prewalk
-  "Like `clojure.walk/prewalk`, but preserves metadata."
-  [f form]
-  (walk (partial prewalk f) identity (f form)))
-
-
 (defn macroexpand-all
   [form]
   (potemkin.walk/prewalk
